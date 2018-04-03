b0a8ff099c0b4c4c688edcab48fdd8105ea850fe
Bodacious <gavin@katanacode.com>
2018-04-03 11:02:03 +0100

    Refactor config class for better readability

--- lib/yodleeicious/config.rb
+++ lib/yodleeicious/config.rb
@@ -1,13 +1,17 @@
-
-
 module Yodleeicious
   class Config
+
     class << self
-      attr_accessor :base_url, :cobranded_username, :cobranded_password, :proxy_url, :logger
+      attr_accessor :base_url
+      attr_accessor :cobranded_app_name
+      attr_accessor :cobranded_username
+      attr_accessor :cobranded_password
+      attr_accessor :proxy_url
+      attr_accessor :logger
     end
-    
-    self.logger = Logger.new(STDOUT)
-    self.logger.level = Logger::WARN  
+
+    self.logger       = Logger.new(STDOUT)
+    self.logger.level = Logger::WARN
 
   end
 end
